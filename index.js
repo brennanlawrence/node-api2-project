5f6a9572743379793ae6f7776719b36c7e672c94
brennanlawrence <brennanlawrence2002@gmail.com>
2021-01-14 08:00:17 -0700

    Added / link

--- index.js
+++ index.js
@@ -5,5 +5,8 @@ const server = express();
 
 server.use("/api/posts", postsRoute);
 
+server.get("/", (req, res) => {
+  res.status(200).json({ message: "Success, welcome to the app!" });
+});
 
 server.listen(5000, () => console.log("Server running on port 5000"));
