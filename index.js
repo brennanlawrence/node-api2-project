19f2c77b0eafc2da5c796549883095aaa10564ef
brennanlawrence <brennanlawrence2002@gmail.com>
2021-01-14 08:10:27 -0700

    Fixed start script

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require("express");
 const postsRoute = require("./posts/postsRouter");
 
 const server = express();
+const port = process.env.PORT || 5000;
 
 server.use("/api/posts", postsRoute);
+
 
 server.get("/", (req, res) => {
   res.status(200).json({ message: "Success, welcome to the app!" });
 });
 
-server.listen(post, () => console.log(`Server running on port: ${port}`));
+server.listen(port, () => console.log(`Server running on port: ${port}`));
